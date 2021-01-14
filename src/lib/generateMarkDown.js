d446b1a8c15c95341387a89c96a2b70e5220072f
alex <alexlismile@163.com>
2021-01-14 14:13:42 +0800

    feat: 实现节本的生成组件

--- src/lib/generateMarkDown.js
+++ src/lib/generateMarkDown.js
@@ -7,41 +7,39 @@ const path = require('path');
 async function genMarkdown(config) {
   const { outDir, inputDir } = config;
   try {
-    return inputDir.map(async (item, index) => {
-        const res = await fs.stat(path.resolve(inputDir[index]));
-        // 是文件
-        if (res.isFile()) {
-            console.log('请检查inputDIr,目录必须是文件夹,不能是文件');
-            return;
-        }
-        // 是文件目录
-        if (res.isDirectory()) {
-            const files = await fs.readdir(path.resolve(inputDir[index]));
+    const res = await fs.stat(path.resolve(inputDir));
+    // 是文件
+    if (res.isFile()) {
+      console.log('请检查inputDIr,目录必须是文件夹,不能是文件');
+      return;
+    }
+    // 是文件目录
+    if (res.isDirectory()) {
+      const files = await fs.readdir(path.resolve(inputDir));
 
-            return files.map(async (f) => {
-                const abs = path.resolve(inputDir[index], f);
-                const source = fs.readFileSync(abs, 'utf-8')
-                // 解析vue模板生成组件md
-                const parserRes = parser(source);
-                const r = new Render(parserRes);
-                const markdownRes = r.renderMarkdown();
-                // 处理vue文件里的示例
-                const example = componentsExampleHandler(parserRes['componentDesc']);
-                const compName = markdownRes.componentName || path.basename(abs, '.vue');
-                const str = formatMarkdown(markdownRes.content, example, compName);
-                // 将md文件写入固定文件夹
-                await writeMDFile(str, compName, outDir);
-                // 返回数据
-                const groupName = markdownRes.groupName;
+      return files.map(async (f) => {
+        const abs = path.resolve(inputDir, f);
+        const source = fs.readFileSync(abs, 'utf-8')
+        // 解析vue模板生成组件md
+        const parserRes = parser(source);
+        const r = new Render(parserRes);
+        const markdownRes = r.renderMarkdown();
+        // 处理vue文件里的示例
+        const example = componentsExampleHandler(parserRes['componentDesc']);
+        const compName = markdownRes.componentName || path.basename(abs, '.vue');
+        const str = formatMarkdown(markdownRes.content, example, compName);
+        // 将md文件写入固定文件夹
+        await writeMDFile(str, compName, outDir);
+        // 返回数据
+        const groupName = markdownRes.groupName;
 
-                return {
-                    compName,
-                    groupName,
-                    content: str
-                }
-            });
+        return {
+          compName,
+          groupName,
+          content: str
         }
-    })
+      });
+    }
   } catch(e) {
     console.log(e);
   }
